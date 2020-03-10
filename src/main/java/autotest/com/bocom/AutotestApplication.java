b4f32fd5722294e08018e527419481cc26963819
李兴利 <lixingli@lixinglideMacBook-Pro.local>
2020-03-10 14:09:32 +0800

    first commit

---
+++ src/main/java/autotest/com/bocom/AutotestApplication.java
@@ -0,0 +1,11 @@
+package autotest.com.bocom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutotestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AutotestApplication.class, args);
+	}
+}
